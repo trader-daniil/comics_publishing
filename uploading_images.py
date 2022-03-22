87eb3ce958cf58b58f25800fa266d7b8ebd33724
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-03-22 11:22:11 +0300

    add exception handler

--- uploading_images.py
+++ uploading_images.py
@@ -7,17 +7,15 @@ from dotenv import load_dotenv
 
 
 def check_error_in_vk_response(vk_response):
-    try:
-        if 'error' in vk_response.json():
-            raise requests.exceptions.RequestException
-    except requests.exceptions.RequestException:
-        return vk_response.json()['error']['error_msg']
-    else:
-        return vk_response
+    if 'error' in vk_response:
+        print(vk_response['error']['error_msg'])
+        raise requests.exceptions.RequestException
 
 
-def get_comic_url():
-    comics_amount = requests.get('https://xkcd.com/info.0.json').json()['num']
+def get_random_comic_url():
+    comic_response = requests.get('https://xkcd.com/info.0.json')
+    comic_response.raise_for_status()
+    comics_amount = comic_response.json()['num']
     comic_num = random.randint(1, comics_amount)
     comic_url = f'https://xkcd.com/{comic_num}/info.0.json'
     return comic_url
@@ -59,20 +57,6 @@ def download_image(image_url, image_path):
         file.write(image_response.content)
 
 
-def get_user_groups(vk_token):
-    vk_groups_url = 'https://api.vk.com/method/groups.get'
-    params = {
-        'access_token': vk_token,
-        'v': '5.81',
-    }
-    response = requests.get(
-        url=vk_groups_url,
-        params=params,
-    )
-    response.raise_for_status()
-    return response.json()
-
-
 def send_image_to_group(group_id, vk_token):
     """
     send image to your vk group
@@ -87,15 +71,12 @@ def send_image_to_group(group_id, vk_token):
         url=vk_wallaper_url,
         params=params,
     )
-    response = check_error_in_vk_response(
-        vk_response=response,
+    response_data = response.json()
+    check_error_in_vk_response(
+        vk_response=response_data,
     )
-    if isinstance(response, str):
-        print(response)
-        return None
     response.raise_for_status()
-    upload_url = response.json()['response']['upload_url']
-    return upload_url
+    return response_data['response']['upload_url']
 
 
 def send_image_to_wall(image_url, vk_token, image_path):
@@ -134,8 +115,12 @@ def save_image_to_group(image_hash, photo, image_server, vk_token, group_id):
         url=saving_url,
         params=params
     )
+    response_data = response.json()
+    check_error_in_vk_response(
+        vk_response=response_data,
+    )
     response.raise_for_status()
-    return response.json()['response'][0]
+    return response_data['response'][0]
 
 
 def publish_image_on_wall(group_id, message, vk_token, attachments):
@@ -155,8 +140,12 @@ def publish_image_on_wall(group_id, message, vk_token, attachments):
         url=publish_url,
         params=params,
     )
+    response_data = response.json()
+    check_error_in_vk_response(
+        vk_response=response_data,
+    )
     response.raise_for_status()
-    return response.json()
+    return response_data
 
 
 def main():
@@ -169,7 +158,7 @@ def main():
         parents=True,
         exist_ok=True,
     )
-    comic_url = get_comic_url()
+    comic_url = get_random_comic_url()
     image_info = parse_image_info(comic_url=comic_url)
     image_url = image_info['image_url']
     author_comment = image_info['author_comment']
@@ -183,12 +172,15 @@ def main():
     )
     vk_token = os.getenv('VK_ACCESS_TOKEN')
     group_id = os.getenv('VK_GROUP_ID')
-    image_url = send_image_to_group(
-        group_id=group_id,
-        vk_token=vk_token,
-    )
-    if not image_url:
+    try:
+        image_url = send_image_to_group(
+            group_id=group_id,
+            vk_token=vk_token,
+        )
+    except requests.exceptions.RequestException:
         return None
+    finally:
+        os.remove(full_image_path)
     try:
         image_info = send_image_to_wall(
             image_url=image_url,
@@ -200,22 +192,32 @@ def main():
     server = image_info['server']
     photo = image_info['photo']
     image_hash = image_info['hash']
-    image_response = save_image_to_group(
-        image_hash=image_hash,
-        photo=photo,
-        image_server=server,
-        vk_token=vk_token,
-        group_id=group_id,
-    )
+    try:
+        image_response = save_image_to_group(
+            image_hash=image_hash,
+            photo=photo,
+            image_server=server,
+            vk_token=vk_token,
+            group_id=group_id,
+        )
+    except requests.exceptions.RequestException:
+        return None
+    finally:
+        os.remove(full_image_path)
     owner_id = image_response['owner_id']
     media_id = image_response['id']
     attachments = f'photo{owner_id}_{media_id}'
-    publish_image_on_wall(
-        group_id=group_id,
-        message=author_comment,
-        vk_token=vk_token,
-        attachments=attachments,
-    )
+    try:
+        publish_image_on_wall(
+            group_id=group_id,
+            message=author_comment,
+            vk_token=vk_token,
+            attachments=attachments,
+        )
+    except requests.exceptions.RequestException:
+        return None
+    finally:
+        os.remove(full_image_path)
 
 
 if __name__ == '__main__':
